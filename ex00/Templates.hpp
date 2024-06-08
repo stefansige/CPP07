2fca8f00c4064c4dd368b916e6934861dd79131c
Stefan <novakovics2001@gmail.com>
2024-06-08 08:18:02 +0200

    finished ex00

---
+++ ex00/Templates.hpp
@@ -0,0 +1,33 @@
+#ifndef TEMPLATE_HPP
+#define TEMPLATE_HPP
+
+#include <iostream>
+#include <string>
+
+template <typename T>
+void swap(T &a, T &b)
+{
+	T temp = a;
+	a = b;
+	b = temp;
+}
+
+template <typename T>
+T min(T a, T b)
+{
+	if (a < b)
+		return a;
+	else
+		return b;
+}
+
+template <typename T>
+T max(T a, T b)
+{
+	if (a > b)
+		return a;
+	else
+		return b;
+}
+
+#endif
